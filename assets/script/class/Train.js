a6a1da8a564c734be48e0ba695ec9cd3e2322680
thomasIRA <thomas.al@live.ca>
2023-10-10 19:34:42 -0400

    ss- bis

--- assets/script/class/Train.js
+++ assets/script/class/Train.js
@@ -13,16 +13,27 @@ export class Train{
         let containerRect = this.#elContainer.getClientRects()[0];
         this.#elContainer.addEventListener('mousemove', (e) => {
 
-            let positionY = e.clientY - containerRect.y;
-            let positionX = e.clientX - containerRect.x;
-            this.#el.style.top = positionY + 'px';
-            this.#el.style.left = positionX + 'px';
-
             let trainRect = this.#el.getClientRects()[0];
             let angle = Math.atan2(e.clientY - trainRect.y, e.clientX - trainRect.x ) * ( 180 / Math.PI );
 
-            this.#el.style.transform = `rotate(${angle}deg)`;
+            let mirror = "";
+            if(angle < -90 || angle > 90){
+                mirror = 'scaleX(-1)';
+                angle = angle - 180;
+            } 
+
+            let transform = `rotate(${angle}deg) ${mirror}`;
+            this.#el.style.transform = transform;
+
             console.log(angle)
+
+            setTimeout(() => {
+                let positionY = e.clientY - containerRect.y;
+                let positionX = e.clientX - containerRect.x;
+                this.#el.style.top = positionY + 'px';
+                this.#el.style.left = positionX + 'px';
+            }, "500");
+
 
         });
     }
